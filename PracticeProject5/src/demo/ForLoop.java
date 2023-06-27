de50e259d8747ea6f5ced53fb682019a82a015be
Divagar Manivasagan <128065016+divagar1102@users.noreply.github.com>
2023-06-27 14:22:54 +0530

    Add files via upload

---
+++ PracticeProject5/src/demo/ForLoop.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class ForLoop {
+	public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Count: " + i);
+        }
+
+        System.out.println("Loop goes to exit");
+    }
+}
+
