077807a72a37be696aaac82e5e81af8d2f28ff95
Divagar Manivasagan <128065016+divagar1102@users.noreply.github.com>
2023-06-27 14:19:39 +0530

    Add files via upload

---
+++ PracticeProject4/src/demo/DoWhileLoop.java
@@ -0,0 +1,14 @@
+package demo;
+
+public class DoWhileLoop {
+	 public static void main(String[] args) {
+	        int count = 1;
+
+	        do {
+	            System.out.println("Count: " + count);
+	            count++;
+	        } while (count <= 5);
+
+	        System.out.println("Loop goes to exit ");
+	    }
+	}
