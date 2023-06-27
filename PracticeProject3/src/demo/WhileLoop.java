998d98aa6b0fa154d5e27441d1291e6d0cfe44ac
Divagar Manivasagan <128065016+divagar1102@users.noreply.github.com>
2023-06-27 14:13:45 +0530

    Add files via upload

---
+++ PracticeProject3/src/demo/WhileLoop.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class WhileLoop {
+	public static void main(String[] args) {
+	        int count = 1;
+	        while (count <= 5) {
+	            System.out.println("Count: " + count);
+	            count++; // Increment the count variable
+	        }
+
+	        System.out.println("While loop has ended");
+	    }
+	}
+
+
