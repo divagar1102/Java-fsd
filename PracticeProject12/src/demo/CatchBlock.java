85eff1020fc715acd4ad818cbde4dea0bb8022bc
divagar1102 <divagar429@gmail.com>
2023-06-29 16:44:16 +0530

    Source Code

---
+++ PracticeProject12/src/demo/CatchBlock.java
@@ -0,0 +1,21 @@
+package demo;
+
+public class CatchBlock {
+	public static void main(String[] args) {
+        try{
+            int a[]=new int[5];
+            System.out.println(a[10]);
+        }
+        catch(ArithmeticException e){
+            System.out.println("Arithmetic exception");
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+           System.out.println("ArrayIndexOutOfBounds exception");
+        }
+        catch(Exception e){
+            System.out.println("Parent exception");
+        }
+        System.out.println("rest of the code");
+    }
+
+}
